8f7b18ada52c8b5a88b3366c2cc7f792d0eb2290
Abrar Jahin Zihan <abrarjahinkhanzihan@gmail.com>
2025-09-09 12:57:53 +0600

    modify the card

--- logic.js
+++ logic.js
@@ -34,24 +34,25 @@ showAllPlants = (data) => {
     data.forEach((element) => {
         singlePlantCard.innerHTML += `
         <div class="card bg-base-100 shadow-sm">
-        <figure>
-            <img src="${element.image}" alt="" class="w-[316px] h-[180px] object-cover rounded-sm" loading="lazy"/>
-        </figure>
-        <div class="card-body gap-2">
-            <h2 class="card-title">${element.name}</h2>
-            <p class="text-[#00000080] overflow-hidden h-10">${element.description}</p>
-            <div class="flex items-center justify-between">
-                <div class="badge bg-[#DCFCE7] text-[#15803D] py-2 px-3 ">${element.category}</div>
-                    <div>
-                        <p class="font-bold">৳${element.price}</p>
+            <figure>
+                <img src="${element.image}" alt="" class="w-[316px] h-[180px] object-cover rounded-sm" loading="lazy"/>
+            </figure>
+            <div class="card-body gap-2">
+                <h2 class="card-title">${element.name}</h2>
+                <p class="text-[#00000080] overflow-hidden h-10">${element.description}</p>
+                <div class="flex items-center justify-between">
+                    <div class="badge bg-[#DCFCE7] text-[#15803D] py-2 px-3 ">${element.category}</div>
+                        <div>
+                            <p class="font-bold">৳${element.price}</p>
+                        </div>
+                    </div>
+                    <div class="card-actions justify-end">
+                        <button id="plant-id-${element.id}"
+                            class="btn bg-[#15803D] text-white w-full rounded-full mt-4 hover:bg-[#3C8A5A]">Add toCart
+                        </button>
                     </div>
                 </div>
-                <div class="card-actions justify-end">
-                    <button id="plant-id-${element.id}"
-                        class="btn bg-[#15803D] text-white w-full rounded-full mt-4 hover:bg-[#3C8A5A]">Add toCart
-                    </button>
-                </div>
-            /div>
+            </div>
         </div>
         `;
     });
